04094dea076fdff4d13b4cdd63a0e51aea053ed9
Dinesh130914 <dineshthiyagu99@gmail.com>
2021-12-29 12:40:28 +0530

    Added:Simple linkedlist -UC1

---
+++ src/com/linkedlist/Runner.java
@@ -0,0 +1,20 @@
+package com.linkedlist;
+
+import java.util.LinkedList;
+
+public class Runner {
+	public static void main(String[] args)
+	{
+		Mylinkedlist mylinkedlist = new Mylinkedlist();
+		
+		mylinkedlist.print();
+		mylinkedlist.addLast(56);
+		mylinkedlist.print();
+		mylinkedlist.addLast(30);
+		mylinkedlist.print();
+		mylinkedlist.addLast(70);
+		mylinkedlist.print();
+		
+	
+	}
+}
